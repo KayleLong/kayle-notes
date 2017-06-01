fa050797eeb7323f9cea2ac899e8ca7df3c11cb8
Kayle <lajxutcm@163.com>
2017-06-01 10:32:05 +0800

    Add Jupyter Notebook

---
+++ python/Jupyter/liaoxuefeng/makeDir.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-05-26 11:41:37
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+with open('dirName.txt', 'r') as f:#open and read file
+	str_dir = f.read()
+
+list_dir = str_dir.split('\n')#split the string, then to list
+for name in list_dir:
+	#makedir
+	#os.mkdir(name)
+	#removedir
+	os.rmdir(name)
